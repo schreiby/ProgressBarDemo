be1073bf2ab57557052cd4f74f85e823d988ba05
Schreiby <ettie.schreiber@gmail.com>
2015-06-07 23:03:15 -0400

    Initial commit

---
+++ app/src/main/java/com/example/ettie/progressbardemo/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.ettie.progressbardemo;
+
+import android.os.AsyncTask;
+import android.os.SystemClock;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ProgressBar;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    private ProgressBar mProgressBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mProgressBar = (ProgressBar) findViewById(R.id.progressBar1);
+        new MyTask().execute();
+    }
+
+    public class MyTask extends AsyncTask<Void, Integer, Void> {
+        @Override
+        protected Void doInBackground(Void... params) {
+            for (int i = 1; i <= 10; i++) {
+                publishProgress(i * 10);
+                SystemClock.sleep(500);
+            }
+            return null;
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            mProgressBar.setProgress(values[0]);
+        }
+    }
+}
+
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.menu_main, menu);
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        // Handle action bar item clicks here. The action bar will
+//        // automatically handle clicks on the Home/Up button, so long
+//        // as you specify a parent activity in AndroidManifest.xml.
+//        int id = item.getItemId();
+//
+//        //noinspection SimplifiableIfStatement
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+//
+//        return super.onOptionsItemSelected(item);
+//    }
+//}
